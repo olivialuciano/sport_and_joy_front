5cbc1ff1bb76ff9b2a0c2894eb75b9366a972d68
Olivia Luciano <olivialuciano2003@gmail.com>
2023-08-22 23:36:29 -0300

    proyecto inicializado, home html basico y css hecho

--- src/App.js
+++ src/App.js
@@ -1,25 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import "./App.css";
+import Home from "./components/Home/Home";
+import Signin from "./components/Singin/Signin";
+import Signup from "./components/Signup/Signup";
 
-function App() {
+const App = () => {
+  const router = createBrowserRouter([
+    { path: "/", element: <Navigate to="/home" /> },
+    {
+      path: "/home",
+      element: <Home />,
+    },
+    {
+      path: "/signin",
+      element: <Signin />,
+    },
+    {
+      path: "/signup",
+      element: <Signup />,
+    },
+  ]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <RouterProvider router={router} />
+    </>
   );
-}
+};
 
 export default App;
---
+++ src/components/Home/Home.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div>
+      <h1 className="brand">Sport&Joy</h1>
+      <h3 className="brand2">¡Reservar una cancha nunca fue tan fácil!</h3>
+      <div className="button-div">
+        <button className="button">ingresar</button>
+        <button className="button">registrarse</button>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
