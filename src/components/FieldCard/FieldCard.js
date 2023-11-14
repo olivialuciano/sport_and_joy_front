0b47ae5055f8e340c8fdf4dbdff88bc1fa2a9d58
Lucila <ansaldilucila@gmail.com>
2023-11-14 17:31:44 -0300

    FieldCard agregado

--- src/components/FieldCard/FieldCard.js
+++ src/components/FieldCard/FieldCard.js
@@ -1,6 +1,17 @@
 import React from "react";
 import "./FieldCard.css";
 
-export const FieldCard = ({ children }) => {
-  return <div>{children}</div>;
+const FieldCard = ({ cancha }) => {
+  return (
+    <div className="tarjeta">
+      <img className="imagen-cancha" src={cancha.imagen} alt={cancha.nombre} />
+      <div className="info-cancha">
+        <h2>{cancha.nombre}</h2>
+        <p className="ubicacion">{cancha.ubicacion}</p>
+        <p className="precio">{cancha.precio}</p>
+      </div>
+    </div>
+  );
 };
+
+export default FieldCard;
